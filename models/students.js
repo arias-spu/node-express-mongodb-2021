9f0a4552bdc4c110105868bf7dd6dca1e90d6da8
Carlos R. Arias <arias@spu.edu>
2021-05-25 21:08:37 -0700

    working on router: post / done

--- models/students.js
+++ models/students.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let StudentSchema = new Schema({
-    first_name: String,
-    last_name: String,
+    firstName: String,
+    lastName: String,
     gpa: Number,
     credits: Number,
     major: String
---
+++ routes/students.js
@@ -0,0 +1,30 @@
+let express = require('express');
+let router = express.Router();
+let StudentSchema = require('../models/students');
+
+function HandleError(response, reason, message, code){
+    console.log('ERROR: ' + reason);
+    response.status(code || 500).json({"error": message});
+}
+
+router.post('/', (request, response, next) =>{
+   let studentJSON = request.body;
+   if (!studentJSON.firstName || !studentJSON.lastName)
+       HandleError(response, 'Missing Information', 'Form Data Missing', 500);
+   else{
+       let student = new StudentSchema({
+           firstName: studentJSON.firstName,
+           lastName: studentJSON.lastName,
+           gpa: studentJSON.gpa || 0,
+           credits : studentJSON.credits || 0,
+           major: studentJSON.major || 'Undecided'
+       });
+       student.save( (error) => {
+           if (error){
+               response.send({"error": error});
+           }else{
+               response.send({"id": student.id});
+           }
+       })
+   }
+});
