a68dbaf84c620c1d4134ac54c2f0df32ea7e4377
Carlos R. Arias <arias@spu.edu>
2021-05-25 19:31:26 -0700

    Added database.js, updated package.json

---
+++ database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const mongoDB = process.env.MONGODB_URI || 'mongodb+srv://arias:SPU2020@mongo-db-0.xycwc.mongodb.net/people?retryWrites=true&w=majority';
+
+mongoose
+    .connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => {
+        console.log('DB Connected!');
+    })
+    .catch(error => {
+        console.log('Connection Error: ${err.message}');
+    });
+
+const db = mongoose.connection;
+
+// Bind the console to errors, to show them on console
+db.on('error', console.error.bind(console, 'MongoDB Connection Error'));
+
+module.exports = db;
